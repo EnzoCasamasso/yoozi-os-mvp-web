f8360d8819624441933092c7f802a02cd7c05256
Enzo Casamasso <enzocasamasso@gmail.com>
2025-03-11 15:29:57 -0300

    ✨ feat(customers-list): added table in customer-list

--- apps/yoozi-os-mvp-web/src/app/domain/customers/pages/customers-list/customers-list.page.ts
+++ apps/yoozi-os-mvp-web/src/app/domain/customers/pages/customers-list/customers-list.page.ts
@@ -1,9 +1,46 @@
 import { Component } from '@angular/core';
 
+import { NzTableModule } from 'ng-zorro-antd/table';
+
 @Component({
   selector: 'yz-customers-list',
-  imports: [],
+  imports: [NzTableModule],
   templateUrl: './customers-list.page.html',
   styleUrl: './customers-list.page.scss',
 })
-export class CustomersListPage {}
+export class CustomersListPage {
+  expandSet = new Set<number>();
+  onExpandChange(id: number, checked: boolean): void {
+    if (checked) {
+      this.expandSet.add(id);
+    } else {
+      this.expandSet.delete(id);
+    }
+  }
+  listOfData = [
+    {
+      id: 1,
+      name: 'John Brown',
+      age: 32,
+      expand: false,
+      address: 'New York No. 1 Lake Park',
+      description: 'My name is John Brown, I am 32 years old, living in New York No. 1 Lake Park.',
+    },
+    {
+      id: 2,
+      name: 'Jim Green',
+      age: 42,
+      expand: false,
+      address: 'London No. 1 Lake Park',
+      description: 'My name is Jim Green, I am 42 years old, living in London No. 1 Lake Park.',
+    },
+    {
+      id: 3,
+      name: 'Joe Black',
+      age: 32,
+      expand: false,
+      address: 'Sidney No. 1 Lake Park',
+      description: 'My name is Joe Black, I am 32 years old, living in Sidney No. 1 Lake Park.',
+    },
+  ];
+}
